11c11df67969fb55947f56a341817e90a3bb4e27
Soleiu <himamegu847@gmail.com>
2020-11-16 00:38:56 +0900

    edit

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -12,6 +12,19 @@ class ListController < ApplicationController
     end
   end 
 
+  def edit
+    @list = List.find_by(id:params[:id])
+  end
+
+  def update
+    @list = List.find_by(id:params[:id])
+    if @list.update_attributes(list_params)
+      redirect_to:root
+    else
+      render action::edit
+    end
+  end
+  
   private
     def list_params
       params.require(:list).permit(:title).merge(user: current_user)
