a7b89431b89c9ab7729ad4f57741c58b775db01a
Soleiu <himamegu847@gmail.com>
2020-11-16 11:08:48 +0900

    add_card_create

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_11_15_210105) do
 
   create_table "cards", force: :cascade do |t|
-    t.string "title", limit: 225, null: false
+    t.string "title", limit: 255, null: false
     t.text "memo", limit: 1000
     t.integer "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
